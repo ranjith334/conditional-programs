e32e99dc50d09d809c1ba904449b6151717b52e8
Ranjith Kumar Punnapu <ranjithprk424@gmail.com>
2025-07-01 14:13:23 +0530

    Add files via upload

---
+++ ifelif.py
@@ -0,0 +1,12 @@
+#if - elif else  statement for largest  number of two a,b
+
+a=int(input("ENter a:"))
+b=int(input("Enter b:"))
+
+if a>b:
+    print("Largest:",a)
+if a<b:
+    print("Smallest",a)    
+else :
+    
+    print("both numbers are equal")    
